9f024db4c6f60d24bfa4d637c56b10f798898f21
Martin Dvorak <ultradvorka@gmail.com>
2013-12-26 01:04:23 +0100

    bin/ > release/

--- bin/deb-push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ../..
-bzr push lp:~ultradvorka/+junk/hh-package
-dput ppa:ultradvorka hh_0.7-1ubuntu2_source.changes
-
-# eof
--- bin/man.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ../man && groff -man -Tascii hh.1
-
-# eof
