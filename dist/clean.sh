a03cb7c692d21b0eda5ddd6c3b8efd1ac254e690
Martin Dvorak <ultradvorka@gmail.com>
2013-12-26 01:05:38 +0100

    release/ > dist/

---
+++ dist/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -vrf ../.git ../.settings ../Debug ../Release ../.cproject ../.project ../.gitignore
+
+rm -vf *.*~ ../*.*~ ../src/*.*~
+rm -rvf ../atom5te.cache
+rm -vf ../man/Makefile ../man/Makefile.in
+rm -vf ../src/Makefile ../src/Makefile.in ../src/hh ../src/*.o 
+rm -rvf ../src/.deps
+rm -vf ../Makefile ../Makefile.in ../aclocal.m4 ../config.log ../config.status ../configure ../depcomp ../install-sh ../missing
+
+# eof
---
+++ dist/dist.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+./clean.sh
+
+cd ..
+aclocal
+automake --force-missing --add-missing
+autoconf
+
+rm -vf depcomp install-sh missing
+cp -vf /usr/share/automake-1.11/depcomp .
+cp -vf /usr/share/automake-1.11/install-sh .
+cp -vf /usr/share/automake-1.11/missing .
+
+cd bin
+
+# eof
