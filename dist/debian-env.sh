25cc46c9c6d3b9a2d5e3c97bfb755ede15cd145f
Martin Dvorak <martin.dvorak@gmail.com>
2014-01-26 23:10:19 +0100

    Debian compliance.

--- dist/debian-env.sh
+++ dist/debian-env.sh
@@ -2,8 +2,9 @@
 
 # Debian release: https://wiki.debian.org/IntroDebianPackaging
 
-export HHVERSION="1.0.6"
-export HHFULLVERSION=${HHVERSION}-1.2
+export HHVERSION="1.0.7"
+#export HHFULLVERSION=${HHVERSION}-1.0 # NMU upload
+export HHFULLVERSION=${HHVERSION}-1 # mantainer upload
 export HH=hh_${HHVERSION}
 export HHRELEASE=hh-${HHFULLVERSION}
 export HHSRC=/home/dvorka/p/hstr/github/hstr
@@ -12,6 +13,6 @@ export HHBUILD=hstr-${NOW}
 
 export UBUNTUVERSION=unstable
 
-export HHBZRMSG="Fixing history management - exact match ensured."
+export HHBZRMSG="Debian compliance."
 
 # eof
