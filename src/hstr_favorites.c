a8337ad952515dedd560d0cd15895ba3321a9148
Martin Dvorak <martin.dvorak@gmail.com>
2014-03-23 03:50:37 +0100

    Fake in-memory favorites work without adding and load/save.

--- src/hstr_favorites.c
+++ src/hstr_favorites.c
@@ -7,28 +7,36 @@
  ============================================================================
 */
 
+#include <string.h>
 #include "include/hstr_favorites.h"
 
 #define FAVORITE_SEGMENT_SIZE 10
 
-void favorites_init(FavoriteItems *favorites) {
+void favorites_init(FavoriteItems *favorites)
+{
 	favorites->items=NULL;
 	favorites->count=0;
 }
 
-void favorites_load(FavoriteItems *favorites) {
+void favorites_load(FavoriteItems *favorites)
+{
 	// TODO fake initialization instead of .hhrc load
-	favorites->count=3;
+	favorites->count=2;
 	favorites->items=malloc(sizeof(char *)*favorites->count);
-	favorites->items[0]="a";
-	favorites->items[1]="b";
-	favorites->items[2]="c";
+	favorites->items[0]=malloc(2);
+	strcpy(favorites->items[0],"a");
+	favorites->items[1]=malloc(2);
+	strcpy(favorites->items[1],"b");
 }
 
-void favorites_add(FavoriteItems *favorites, char *newFavorite) {
+void favorites_add(FavoriteItems *favorites, char *newFavorite)
+{
 	favorites->items=realloc(favorites->items, sizeof(char *)*favorites->count);
 	favorites->items[favorites->count++]=newFavorite;
+
+	favorites_save(favorites);
 }
 
-void favorites_save(FavoriteItems *favorites) {
+void favorites_save(FavoriteItems *favorites)
+{
 }
