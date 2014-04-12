4b27f038515b64bc9972cd44537836d3d0d87b06
Martin Dvorak <martin.dvorak@gmail.com>
2014-04-12 07:37:50 +0200

    Adding test plan for escaped characters.

---
+++ tests/src/test_escape_chars.c
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : test_escape_chars.c
+ Author      : martin.dvorak@mindforger.com
+ Copyright   : Apache 2.0
+ Description : A test for verification of escape/special chars handling
+ ============================================================================
+*/
+
+void generate_bash_history_with_special_chars() {
+	/*
+	 // crash
+	 %s
+
+	 // substituation > should not happen:
+	 date -d yesterday +%A
+     date -d yesterday +0X1.4C41E000008P-895
+
+     // crash
+     mocp -Q "<%state> '%file'\n"
+	 */
+}
+
+void main() {
+	generate_bash_history_with_special_chars();
+}
+
