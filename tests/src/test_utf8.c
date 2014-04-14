05b07db197c4939070658278234c57215fc7a41f
Martin Dvorak <martin.dvorak@gmail.com>
2014-04-14 22:02:28 +0200

    Just sync diff (switching branches) - not compilable.

---
+++ tests/src/test_utf8.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : test_utf8.c
+ Author      : martin.dvorak@mindforger.com
+ Copyright   : Apache 2.0
+ Description : A test
+ ============================================================================
+*/
+
+#include <string.h>
+#include <stdio.h>
+
+#define LINELNG 500
+
+int main(int argc, char *argv[])
+{
+	// UTF8 test: czech & chinese
+	// hf @ SF.net as inspiration
+}
