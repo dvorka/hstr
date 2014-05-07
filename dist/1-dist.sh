c6b87f87fe82c5ebbc9d51e5c1472a7e040d1931
Martin Dvorak <martin.dvorak@gmail.com>
2014-05-07 22:54:39 +0200

    Fixing Ubuntu release automation.

--- dist/1-dist.sh
+++ dist/1-dist.sh
@@ -11,6 +11,7 @@ rm -rvf autom*te.cache
 
 if [ -d /usr/share/automake-1.11 ]
 then
+    rm -vf depcomp install-sh missing
     cp -vf /usr/share/automake-1.11/depcomp .
     cp -vf /usr/share/automake-1.11/install-sh .
     cp -vf /usr/share/automake-1.11/missing .
