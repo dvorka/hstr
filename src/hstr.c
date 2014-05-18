a1d1681aad1a84ca63e384d4e713f7eb4db0396a
Martin Dvorak <martin.dvorak@gmail.com>
2014-05-18 05:51:32 +0200

    Source code polishing

--- src/hstr.c
+++ src/hstr.c
@@ -688,6 +688,7 @@ void loop_to_select(Hstr *hstr)
 	keypad(stdscr, TRUE);
 	noecho();
 	color_start();
+	// TODO move the code below to hstr_curses
 	color_init_pair(HH_COLOR_NORMAL, -1, -1);
 	if(hstr->hicolor) {
 		color_init_pair(HH_COLOR_HIROW, COLOR_WHITE, COLOR_GREEN);
