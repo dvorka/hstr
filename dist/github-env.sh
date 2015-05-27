863ed12761ab4412eb7cb07037c05cefdacc7a0b
Martin Dvorak <martin.dvorak@mindforger.com>
2015-05-27 23:16:00 +0200

    Fixing GitHub release script version.

--- dist/github-env.sh
+++ dist/github-env.sh
@@ -4,6 +4,6 @@ export NOW=`date +%Y-%m-%d--%H-%M-%S`
 export GH_RELEASE_DIR=~/p/hstr/release
 export GH_DISTRO_DIR=${GH_RELEASE_DIR}/release-${NOW}
 
-export HHVERSION="1.17"
+export HHVERSION="1.18"
 
 # eof
