68b99d166c94ad1641b3dbf8b6d909533a4b3b55
Martin Dvorak <martin.dvorak@mindforger.com>
2016-04-27 08:13:30 +0200

    Adding Launchpad release protection from running and destroying local git repository.

--- dist/ubuntu-make-all-distros.sh
+++ dist/ubuntu-make-all-distros.sh
@@ -9,9 +9,15 @@
 # - check minor version in this file
 # - run this script from ~/p/hstr/launchpad
 
-export HHBZRMSG="Buffer overflow fix of regexp mode."
+if [ -e "../.git" ]
+then
+  echo "This script MUST NOT be run from Git repository - run it e.g. from ~/p/hstr/launchpad instead"
+  exit 1
+fi
+
+export HHBZRMSG="Top and bottom prompt."
 export VERSION=1.20.
-export MINOR=1
+export MINOR=3
 
 # precise quantal saucy precise utopic / trusty vivid wily xenial
 for DISTRO in xenial
