8db5b66fd6a7f38fa284d3b1f364617b9128e415
Martin Dvorak <martin.dvorak@mindforger.com>
2018-07-31 23:23:20 +0200

    WIP tarball build script.

--- build/tarball/tarball-build.sh
+++ build/tarball/tarball-build.sh
@@ -13,52 +13,37 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Method:
+# 1. Tarball release checklist:
+#   - git grep <previous version>
+#   - update version in the source code (hstr.c)
+#   - update version in configure.ac
+#   - update version github-env.sh
+#   - ... search for old version using Eclipse/grep
+#
+# 2. Run this script from this directory.
+#
 
-export AUTOMAKE_LIB=`automake --print-libdir`
+. ./tarball-env.sh
+export SCRIPT_HOME=`pwd`
 
-./clean.sh
-
-cd ..
-aclocal
-automake --force-missing --add-missing
-autoconf
-
-rm -rvf autom*te.cache config.sub config.guess
-
-if [ -d ${AUTOMAKE_LIB} ]
-then
-    rm -vf compile depcomp install-sh missing
-    cp -vf ${AUTOMAKE_LIB}/compile .
-    cp -vf ${AUTOMAKE_LIB}/depcomp .
-    cp -vf ${AUTOMAKE_LIB}/install-sh .
-    cp -vf ${AUTOMAKE_LIB}/missing .
-    # platform specific detection in configure.ac
-    cp -vf ${AUTOMAKE_LIB}/config.sub .
-    cp -vf ${AUTOMAKE_LIB}/config.guess .
-else 
-    echo "****************************************************************"
-    echo "Error: automake ${AUTOMAKE_LIB} not found - exiting!"
-    exit 1
-fi
-
-# detect Bash@Ubuntu@Windows
-export VENDOR_FILE=/proc/version
-export HINT_FILE=/tmp/hh-ms-wsl
-
-if [ -e "${VENDOR_FILE}" ]
-then
-    IS_MS=`grep Microsoft ${VENDOR_FILE}`
-    if [[ ! -z "${IS_MS}" ]]
+function makeTarballRelease() {
+    cp -vrf ${SCRIPT_HOME}/../../hstr .
+    cd hstr && rm -vrf debian doc tests && cd dist && ./1-dist.sh
+    if [ $? -ne 0 ]
     then
-	echo "DETECTED Bash on Linux on Windows (MS WSL)"
-	touch ${HINT_FILE}
-    else
-	rm -vf ${HINT_FILE}
+       exit 1;
     fi
-else
-    rm -vf ${HINT_FILE}
-fi
+    cd ../..
+    tar zcfv hh-${HHVERSION}-src.tgz hstr
+    cd hstr && ./configure && make && cp src/hh ..
+    cd ..
+    tar zcfv hh-${HHVERSION}-bin-64b.tgz hh
+}
 
-cd dist
+
+mkdir -v ${GH_DISTRO_DIR}
+cd ${GH_DISTRO_DIR} && makeTarballRelease
 
 # eof
